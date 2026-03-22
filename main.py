fe2aac82c909ed2360d56b36b570b53d6d4d8fec
Ekaterina Parkhacheva <parkhacheva.05@mail.ru>
2026-03-22 22:18:36 +0300

    update-14

--- main.py
+++ main.py
@@ -1,23 +1,17 @@
 from kivymd.app import MDApp
 from kivymd.uix.screen import MDScreen
-from kivymd.uix.button import MDButton, MDButtonText
-
+from kivymd.uix.button import MDRaisedButton
 
 class GliderApp(MDApp):
     def build(self):
         self.theme_cls.theme_style = "Dark"
         self.theme_cls.primary_palette = "Orange"
 
-        return (
-            MDScreen(
-                MDButton(
-                    MDButtonText(
-                        text="Hello, World",
-                    ),
-                    pos_hint={"center_x": 0.5, "center_y": 0.5},
-                )
+        return MDScreen(
+            MDRaisedButton(
+                text="Hello, World",
+                pos_hint={"center_x": 0.5, "center_y": 0.5},
             )
         )
-
 
 GliderApp().run()
